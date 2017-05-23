dd5db1149bc03a110da0b6c375b9f397ae05dca3
JoeTV81 <Joenumber81@gmail.com>
2017-05-23 16:47:05 -0700

    Front end skeleton and some of the back end

---
+++ property/property.controller.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app')
+        .controller('PropertyController', PropertyController);
+
+    PropertyController.$inject = [''];
+    function PropertyController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
---
+++ user/user.controller.js
@@ -0,0 +1,19 @@
+(function() {
+'use strict';
+
+    angular
+        .module('app')
+        .controller('UserController', UserController);
+
+    UserController.$inject = [''];
+    function UserController() {
+        var vm = this;
+        
+
+        activate();
+
+        ////////////////
+
+        function activate() { }
+    }
+})();
